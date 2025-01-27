a59e21b30d1debd590187fd5eabf8e108e8280a4
Tirth Patel <tirthvpatel2003@gmail.com>
2025-01-27 13:14:58 +1100

    added vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex bg-slate-400 justify-center items-center w-screen h-screen'>
+        HEllo
+      </div>
+    </>
+  )
+}
+
+export default App
