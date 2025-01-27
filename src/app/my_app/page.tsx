8709dbe51ad96244878d1fab42ac76c185b83d60
Tirth Patel <tirthvpatel2003@gmail.com>
2025-01-27 14:35:06 +1100

    added next project

---
+++ src/app/my_app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <div className="flex bg-slate-700 h-screen w-screen text-white justify-center items-center text-4xl">
+      Hello
+    </div>
+  );
+}
