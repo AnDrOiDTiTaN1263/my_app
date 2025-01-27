5a6067354705b031eaaf569c466d90cec236e015
Tirth Patel <tirthvpatel2003@gmail.com>
2025-01-27 14:54:43 +1100

    removed myap

--- src/app/my_app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Page() {
-  return (
-    <div className="flex bg-slate-700 h-screen w-screen text-white justify-center items-center text-4xl">
-      Hello
-    </div>
-  );
-}
