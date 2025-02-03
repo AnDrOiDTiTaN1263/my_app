010300371f916c05496e055e6edbdb808164ec8f
Tirth Patel <tirthvpatel2003@gmail.com>
2025-02-03 13:50:01 +1100

    testing hosting with amplify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='flex w-screen h-screen justify-center items-center'>T's Website hosted by amplify</div>
+    <div className='flex w-screen h-screen justify-center items-center'>Website hosted by amplify</div>
   )
 }
